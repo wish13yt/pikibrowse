72b8889279e84220602202b6800c371313b536a1
Wish <100239673+wish13yt@users.noreply.github.com>
2024-09-19 19:53:09 -0500

    add json file creation, wip 	new file:   .gitignore 	modified:   pikibrowse.py

--- pikibrowse.py
+++ pikibrowse.py
@@ -3,3 +3,8 @@ print("PikiBrowse")
 user = input("Please enter the Pikidiary username you wish to search.")
 piki = requests.get('https://pikidiary.lol/api/posts/' + user)
 print(piki.content)
+# stuff below has not been fully tested, use with caution!
+output = piki.content.decode()
+f = open("user.json", "a")
+f.write(output)
+f.close()
